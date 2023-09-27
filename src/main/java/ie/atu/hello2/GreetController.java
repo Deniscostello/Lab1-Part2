ac0682e500331b91b1e9f20ecd098ba0ce9e8dac
Denis Costello <G00396309@atu.ie>
2023-09-27 10:38:42 +0100

    Path variable

--- src/main/java/ie/atu/hello2/GreetController.java
+++ src/main/java/ie/atu/hello2/GreetController.java
@@ -1,5 +1,13 @@
 package ie.atu.hello2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GreetController {
+    @GetMapping("/greet/{name}")
+    public String name(@PathVariable String name){
+        return "Hello" + name;
+    }
 }
