04094acc2c8a73616b908a3e4848a2a172cf910b
Denis Costello <G00396309@atu.ie>
2023-09-27 10:24:56 +0100

    Added get mapping hello

--- src/main/java/ie/atu/hello2/RequestController.java
+++ src/main/java/ie/atu/hello2/RequestController.java
@@ -1,4 +1,12 @@
 package ie.atu.hello2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RequestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
 }
